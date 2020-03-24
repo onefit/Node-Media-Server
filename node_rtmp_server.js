71740ff49e580c2519f08b1d184fb1603adfd950
arielweinberger <weinberger.ariel@gmail.com>
2020-03-24 13:50:33 +0100

    Add TCP server logs

--- node_rtmp_server.js
+++ node_rtmp_server.js
@@ -17,6 +17,7 @@ class NodeRtmpServer {
   constructor(config) {
     config.rtmp.port = this.port = config.rtmp.port ? config.rtmp.port : RTMP_PORT;
     this.tcpServer = Net.createServer((socket) => {
+      console.log('Created new tcp server');
       let session = new NodeRtmpSession(config, socket);
       session.run();
     })
