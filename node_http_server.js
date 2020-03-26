35c89c20a6df362f5ea963bbeef21861fa142dc7
arielweinberger <weinberger.ariel@gmail.com>
2020-03-26 15:29:17 +0100

    Cors and cors, cors.

--- node_http_server.js
+++ node_http_server.js
@@ -36,13 +36,13 @@ class NodeHttpServer {
     app.use(cors());
     console.log('CORS enabled');
 
-    // app.all('*', (req, res, next) => {
-    //   res.header("Access-Control-Allow-Origin", this.config.http.allow_origin);
-    //   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-    //   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    //   res.header("Access-Control-Allow-Credentials", true);
-    //   req.method === "OPTIONS" ? res.sendStatus(200) : next();
-    // });
+    app.all('*', (req, res, next) => {
+      res.header("Access-Control-Allow-Origin", this.config.http.allow_origin);
+      res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+      res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+      res.header("Access-Control-Allow-Credentials", true);
+      req.method === "OPTIONS" ? res.sendStatus(200) : next();
+    });
 
     app.get('*.flv', (req, res, next) => {
       req.nmsConnectionType = 'http';
