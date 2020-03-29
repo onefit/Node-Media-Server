5793cc879c8ed893268839908f3e290baa4c0c05
Guy Rombaut <guy@one.fit>
2020-03-29 13:53:36 +0200

    Update node_trans_session.js

--- node_trans_session.js
+++ node_trans_session.js
@@ -65,6 +65,7 @@ class NodeTransSession extends EventEmitter {
     Array.prototype.push.apply(argv, this.conf.acParam);
     Array.prototype.push.apply(argv, ['-f', 'tee', '-map', '0:a?', '-map', '0:v?', mapStr]);
     argv = argv.filter((n) => { return n }); //去空
+    Logger.ffdebug(`FF输出：${argv}`);
     this.ffmpeg_exec = spawn(this.conf.ffmpeg, argv);
     this.ffmpeg_exec.on('error', (e) => {
       Logger.ffdebug(e);
